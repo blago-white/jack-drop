9c03b0ba07d06790a69704c144699cfde853f8df
Bogdan Loginov <bogdanloginov31@gmail.com>
2024-03-24 11:59:15 +0300

    Add sqlite3 test db

--- backend/products/products/common/settings.py
+++ backend/products/products/common/settings.py
@@ -90,6 +90,10 @@ DATABASES = {
         'USER': os.environ.get("POSTGRES_USER"),
         'PASSWORD': os.environ.get("POSTGRES_PASSWORD"),
         'HOST': os.environ.get("POSTGRES_HOST"),
+    },
+    'test': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
