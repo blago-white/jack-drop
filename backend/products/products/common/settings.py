3d1c83099793acaa29923060de730ca693b0ac97
blago-white <bogdanloginov31@gmail.com>
2024-03-29 13:46:21 +0300

    Add docker

--- backend/products/products/common/settings.py
+++ backend/products/products/common/settings.py
@@ -160,6 +160,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = BASE_DIR / 'static'
+
 MEDIA_URL = 'uploads/'
 
 MEDIA_ROOT = BASE_DIR
---
+++ backend/products/products/start.sh
@@ -0,0 +1,7 @@
+python manage.py collectstatic --noinput
+
+python manage.py makemigrations --noinput
+
+python manage.py migrate
+
+gunicorn common.wsgi:application --bind 0.0.0.0
