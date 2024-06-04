590b7a8812fa9d0364e43dc6cfbb64ef889b3a7d
blago-white <bogdanloginov31@gmail.com>
2024-06-04 19:28:52 +0300

    Complete main page

---
+++ backend/core/core/static/js/header.js
@@ -0,0 +1,27 @@
+const headerButton = document.getElementById('menu-btn');
+const header = document.getElementById('header');
+let toggled = false;
+
+function toggleMenu(event) {
+    header.style.minHeight = toggled ? "9.25vh" : "calc(100vw*calc(597/960))";
+
+    setTimeout(() => {
+        if (toggled) {
+            document.getElementById('header').appendChild(document.getElementById('navbar'));
+            document.getElementById('navbar').style.maxHeight = "100%";
+            document.getElementById('navbar').style.maxWidth = "100%";
+            document.getElementById('navbar').style.display = "flex";
+            document.getElementById('menu-btn').classList.toggle('sandwich-menu-tggled');
+        } else {
+            document.getElementById('navbar').style.maxHeight = "0%";
+            document.getElementById('navbar').style.maxWidth = "0%";
+            document.getElementById('navbar').style.display = "none";
+
+            document.getElementById('menu-btn').classList.toggle('sandwich-menu-tggled');
+        }
+    }, 0.2)
+
+    toggled = !toggled;
+}
+
+headerButton.addEventListener("click", toggleMenu);
