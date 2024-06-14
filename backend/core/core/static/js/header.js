8fb3d6ad18604442bef7f2fe59e3ae8017f08f76
Bogdan Loginov <bogdanloginov31@gmail.com>
2024-06-14 20:25:30 +0300

    Small updates in layout

--- backend/core/core/static/js/header.js
+++ backend/core/core/static/js/header.js
@@ -3,23 +3,30 @@ const header = document.getElementById('header');
 let toggled = false;
 
 function toggleMenu(event) {
-    header.style.minHeight = toggled ? "9.25vh" : "calc(100vw*calc(450/960))";
+    header.style.minHeight = toggled ? "9.25vh" : "46.875vw";
+
+    document.getElementById('menu-btn').classList.toggle('sandwich-menu-tggled');
+
+    if (!toggled) {
+        document.getElementById('header').appendChild(document.getElementById('navbar'));
+        document.getElementById('navbar').style.display = "none";
+        document.getElementById('navbar').style.maxHeight = "100%";
+        document.getElementById('navbar').style.maxWidth = "100%";
+    } else {
+        console.log("switch");
+        document.getElementById('navbar').style.maxHeight = "0vh";
+        document.getElementById('navbar').style.maxWidth = "0%";
+    }
 
     setTimeout(() => {
         if (toggled) {
-            document.getElementById('header').appendChild(document.getElementById('navbar'));
-            document.getElementById('navbar').style.maxHeight = "100%";
-            document.getElementById('navbar').style.maxWidth = "100%";
             document.getElementById('navbar').style.display = "flex";
-            document.getElementById('menu-btn').classList.toggle('sandwich-menu-tggled');
         } else {
-            document.getElementById('navbar').style.maxHeight = "0%";
-            document.getElementById('navbar').style.maxWidth = "0%";
+            console.log("tim");
             document.getElementById('navbar').style.display = "none";
-
-            document.getElementById('menu-btn').classList.toggle('sandwich-menu-tggled');
+            document.getElementById('main-part-h').appendChild(document.getElementById('navbar'));
         }
-    }, 0.2)
+    }, 200)
 
     toggled = !toggled;
 }
