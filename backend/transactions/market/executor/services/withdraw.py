7440adec33db73aa745f4398883b6044d223b425
Bogdan Loginov <bogdanloginov31@gmail.com>
2024-07-11 16:31:24 +0300

    Add RPS constraint

--- backend/transactions/market/executor/services/withdraw.py
+++ backend/transactions/market/executor/services/withdraw.py
@@ -1,4 +1,7 @@
 import asyncio
+import math
+
+import time
 
 import aiohttp
 from django.conf import settings
@@ -9,17 +12,30 @@ from .transfer import WithdrawResult
 
 
 class ItemWithdrawService(BaseMarketApiService):
+    _last_request_duration: float
+    _max_RPS: int = 5
+
     async def bulk_withdraw(
             self, items: list[ScheduledItem]
     ) -> tuple[bool, list[int]]:
-        tasks = [self.withdraw(item=item) for item in items]
 
-        results: list[WithdrawResult] = await asyncio.gather(*tasks)
+        results = []
+        request_min_duration = round((10*3) / 5, 3) + 20
+
+        for item in items:
+            results.append(await self.withdraw(item=item))
+
+            if self._last_request_duration < request_min_duration:
+                await asyncio.sleep(
+                    request_min_duration - self._last_request_duration
+                )
 
         return (all([i.success for i in results]),
                 [i.inventory_item_id for i in results if not i.success])
 
     async def withdraw(self, item: ScheduledItem) -> WithdrawResult:
+        start = time.time()
+
         async with aiohttp.ClientSession() as session:
             async with session.post(
                 url=settings.WITHDRAW_MARKET_ENDPOINT_URL.format(
@@ -30,6 +46,8 @@ class ItemWithdrawService(BaseMarketApiService):
                 )
             ) as response:
                 result = await response.json()
+
+        self._last_request_duration = (time.time() - start) * 1000
 
         return WithdrawResult(
             success=result.get("success"),
