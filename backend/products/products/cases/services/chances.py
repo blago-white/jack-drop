1e4ae999ac256eb403042a1d46b3afe58aadacf3
Bogdan <bogdanloginov31@gmail.com>
2024-09-11 22:32:37 +0300

    Fix last commit

--- backend/products/products/cases/services/chances.py
+++ backend/products/products/cases/services/chances.py
@@ -45,7 +45,7 @@ class CaseItemsChancesService(BaseCaseItemsChancesService):
             self,
             items_of_case: models.QuerySet[Item]
             ) -> list[int]:
-        if not items:
+        if not items_of_case:
             return []
 
         pricing: list[float] = items_of_case.values_list(
