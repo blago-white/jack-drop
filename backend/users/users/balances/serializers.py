5b4f0077f74c3a92e38fdd4db46db01a64b22aa8
Bogdan Loginov <bogdanloginov31@gmail.com>
2025-01-14 00:14:53 +0300

    Update min deposit amount

--- backend/users/users/balances/serializers.py
+++ backend/users/users/balances/serializers.py
@@ -22,7 +22,7 @@ class ClientDepositSerializer(serializers.ModelSerializer):
         required=False
     )
 
-    amount = serializers.FloatField(min_value=100)
+    amount = serializers.FloatField(min_value=499)
 
     promocode = serializers.CharField(max_length=16,
                                       allow_null=True,
