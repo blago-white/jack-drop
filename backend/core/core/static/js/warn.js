aeb848e10c46b653c8bd8c8b59c2e7e98c20d416
Bogdan Loginov <bogdanloginov31@gmail.com>
2025-02-21 15:58:52 +0300

    Add new warns

--- backend/core/core/static/js/warn.js
+++ backend/core/core/static/js/warn.js
@@ -1,11 +1,12 @@
 function makeWarn(header, desc) {
+    const desc = `<span style="grid-row: 1;grid-column: 1;max-width: 75%;text-align: center;">${desc}</span>` if desc else ''
+
     document.getElementById('prize-wrappper').innerHTML = `
         <div style="max-width: 90%;display: grid;grid-template-rows: 1fr;grid-template-columns: 1fr;justify-items: center;align-items: center;transform: scale(0);transition: all .2s ease;" id="warn-content">
             <img src="https://s.iimg.su/s/21/HgBazUEhROqX0TutqYFK0sQWtWSAFjunhOz0E4Kr.png" style="grid-row: 1;grid-column: 1;width: 100%;">
             <div class="warn-content" style="grid-row: 1;grid-column: 1;display: flex;flex-direction: column;align-items: center;">
                 <h2 style="font-family: 'Gilroy Bold';font-size: 2em;max-width: 75%;text-align: center;">${header}</h2>
-
-                ${`<span style="grid-row: 1;grid-column: 1;max-width: 75%;text-align: center;">${desc}</span>` if desc else ''}
+                ${desc}
                 <button class="super-button" onclick="clearWarn()" style="margin-top: calc(0.83* 2em);">
                     <span class="super-button-bg"></span>
                     <span class="super-button-text">Окей</span>
