0c337528440349aee92cc78e998c838a41b0d475
Bogdan Loginov <bogdanloginov31@gmail.com>
2025-02-21 15:40:32 +0300

    Update styles and layouts

--- backend/core/core/static/js/warn.js
+++ backend/core/core/static/js/warn.js
@@ -1,23 +1,30 @@
 function makeWarn(header, desc) {
     document.getElementById('prize-wrappper').innerHTML = `
-<div style="display: grid;grid-template-rows: 1fr;grid-template-columns: 1fr;justify-items: center;align-items: center;">
-<img src="https://s.iimg.su/s/21/HgBazUEhROqX0TutqYFK0sQWtWSAFjunhOz0E4Kr.png" style="grid-row: 1;grid-column: 1;width: 100%;">
-<div class="warn-content" style="grid-row: 1;grid-column: 1;display: flex;flex-direction: column;align-items: center;">
-    <h2 style="font-family: 'Gilroy Bold';font-size: 2em;">${header}</h2>
-<span style="grid-row: 1;grid-column: 1;">${desc}</span>
-<button class="super-button" onclick="clearWarn()" style="margin-top: calc(0.83* 2em);">
-    <span class="super-button-bg"></span>
-    <span class="super-button-text">Окей</span>
-</button></div></div>
-    `
+        <div style="display: grid;grid-template-rows: 1fr;grid-template-columns: 1fr;justify-items: center;align-items: center;transform: scale(0);transition: all .2s ease;" id="warn-content">
+            <img src="https://s.iimg.su/s/21/HgBazUEhROqX0TutqYFK0sQWtWSAFjunhOz0E4Kr.png" style="grid-row: 1;grid-column: 1;width: 100%;">
+            <div class="warn-content" style="grid-row: 1;grid-column: 1;display: flex;flex-direction: column;align-items: center;">
+                <h2 style="font-family: 'Gilroy Bold';font-size: 2em;">${header}</h2>
+                <span style="grid-row: 1;grid-column: 1;">${desc}</span>
+                <button class="super-button" onclick="clearWarn()" style="margin-top: calc(0.83* 2em);">
+                    <span class="super-button-bg"></span>
+                    <span class="super-button-text">Окей</span>
+                </button>
+            </div>
+        </div>
+    `;
 
     document.getElementById("prize-wrappper").style.display = 'flex';
     document.getElementById("prize-wrappper").style.visibility = 'visible';
     document.getElementById("prize-wrappper").style.background = 'rgb(20, 20, 20, .5)';
+    document.getElementById("warn-content").style.transform = 'none';
 }
 
 function clearWarn() {
-    document.getElementById('prize-wrappper').innerHTML = '';
-    document.getElementById('prize-wrappper').style.background = 'none';
-    document.getElementById("prize-wrappper").style.visibility = 'hidden';
+    document.getElementById("warn-content").style.transform = 'scale(0)';
+
+    setTimeout(() => {
+        document.getElementById('prize-wrappper').style.background = 'none';
+        document.getElementById("prize-wrappper").style.visibility = 'hidden';
+        document.getElementById('prize-wrappper').innerHTML = '';
+    })
 }
