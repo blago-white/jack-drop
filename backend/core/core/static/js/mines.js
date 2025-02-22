cf4e13fd01e0d9a9a751d2e7a1ed26c0e7a35b58
Bogdan Loginov <bogdanloginov31@gmail.com>
2025-02-22 14:54:26 +0300

    Update mines front end

--- backend/core/core/static/js/mines.js
+++ backend/core/core/static/js/mines.js
@@ -89,15 +89,15 @@ async function iterField(id) {
         `;
         const factorLabel = document.getElementById("factor-label");
 
+        gameFactor = (gameFactor + response.next_win_factor).toFixed(3);
+
         if (factorLabel) {
             factorLabel.innerHTML = `
-                ${response.next_win_factor}<span style="color: #aaa;">X</span>
+                ${gameFactor}<span style="color: #aaa;">X</span>
             `
         } else {
             document.getElementById('mines-game-form').innerHTML = `<h1 style="color: #0047FF;background-image: linear-gradient(90deg, #0047FF 0%, #FF007A 35%);color: transparent;-webkit-background-clip: text;background-clip: text;margin: 0px;font-size: 6em;
             ">...<span style="color: #aaa;">X</span></h1>` + document.getElementById('mines-game-form').innerHTML;
-
-            gameFactor = (gameFactor + response.next_win_factor).toFixed(3);
 
             document.getElementById("factor-label").innerHTML = `
                 ${gameFactor}<span style="color: #aaa;">X</span>
