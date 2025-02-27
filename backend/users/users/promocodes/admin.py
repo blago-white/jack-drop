f3cef79e999bf8e2fc7b282aedf15104d5a14490
Bogdan Loginov <bogdanloginov31@gmail.com>
2025-02-27 23:26:20 +0300

    Update admin pannel

--- backend/users/users/promocodes/admin.py
+++ backend/users/users/promocodes/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 
-from .models import Promocode
+from .models import Promocode, PersonalDepositOffer
 
 
 @admin.register(Promocode)
 class PromocodeAdmin(admin.ModelAdmin):
     list_display = ["code", "discount", "for_personal_offers"]
+
+
+@admin.register(PersonalDepositOffer)
+class PersonalDepositOfferAdmin(admin.ModelAdmin):
+    list_display = ["recipient", "activated", "blocked"]
