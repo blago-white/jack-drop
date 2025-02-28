370b9699a0c3fb3a862f03df3f630b9b9e121ace
Bogdan Loginov <bogdanloginov31@gmail.com>
2025-02-28 18:48:52 +0300

    Fix users service

--- backend/users/users/balances/repositories/deposits.py
+++ backend/users/users/balances/repositories/deposits.py
@@ -59,7 +59,7 @@ class DepositRepository(BaseRepository):
                     print("OFFER PROMO ERROR", e)
                     discount = 0
                 else:
-                    used_promo, discount = for_personal_offers.code, for_personal_offers.discount
+                    used_promo, discount = for_personal_offers, for_personal_offers.discount
 
             else:
                 discount, used_promo, promo_blogger = self._promocodes_service.use(
