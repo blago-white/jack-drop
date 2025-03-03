a24ed3da8ee00ca519c3159aec6393a8c448f69d
Bogdan Loginov <bogdanloginov31@gmail.com>
2025-03-03 20:12:03 +0300

    Some fixes

--- backend/transactions/payments/transactions/api/urls.py
+++ backend/transactions/payments/transactions/api/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 
-from .endpoints import NicepayInitReplenishApiView, NicePayTransactionCallbackApiView, \
-    TransactionValidationApiView, SkinifyTransactionCallbackApiView
+from .endpoints import (NicepayInitReplenishApiView,
+                        NicePayTransactionCallbackApiView,
+                        TransactionValidationApiView,
+                        SkinifyTransactionCallbackApiView,
+                        SkinifyInitReplenishApiView)
 
 urlpatterns = [
     path("create/", NicepayInitReplenishApiView.as_view(), name="init"),
+    path("create-skinify/", SkinifyInitReplenishApiView.as_view(), name="init"),
     path("callback/", NicePayTransactionCallbackApiView.as_view(), name="callback-nicepay"),
     path("callback-skinify/", SkinifyTransactionCallbackApiView.as_view(), name="callback-skinify"),
     path("validate/", TransactionValidationApiView.as_view(), name="validate"),
