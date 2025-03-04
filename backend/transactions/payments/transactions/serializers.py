b8475d4ea7d4c74b2db0f131ee313ed1ff1bccb5
Bogdan Loginov <bogdanloginov31@gmail.com>
2025-03-04 14:38:56 +0300

    Update serializers

--- backend/transactions/payments/transactions/serializers.py
+++ backend/transactions/payments/transactions/serializers.py
@@ -2,13 +2,13 @@ from rest_framework import serializers
 
 
 class TransactionCreationPubllicSerializer(serializers.Serializer):
-    amount = serializers.FloatField(min_value=500)
+    amount = serializers.FloatField(min_value=0)
 
 
 class TransactionCreationSerializer(serializers.Serializer):
     user_id = serializers.IntegerField()
     user_login = serializers.CharField(max_length=200)
-    amount = serializers.FloatField(min_value=500)
+    amount = serializers.FloatField(min_value=0)
     promocode = serializers.CharField(max_length=16,
                                       allow_null=True,
                                       allow_blank=True)
