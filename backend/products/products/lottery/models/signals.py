507b4ba7c3c6cd480f5a93981a0d68ada9a65943
Bogdan Loginov <bogdanloginov31@gmail.com>
2025-03-09 16:03:23 +0300

    Updates

--- backend/products/products/lottery/models/signals.py
+++ backend/products/products/lottery/models/signals.py
@@ -1,10 +1,20 @@
 import datetime
+import threading
+
 import time
 
 from django.db.models.signals import post_save
 
 from .lottery import LotteryEvent
 from ..tasks import implement_lottery
+
+
+def _apply_lottery(delay: int):
+    time.sleep(delay)
+
+    print("START LOTTERY")
+
+    implement_lottery()
 
 
 def implement_lottery_post_save(
@@ -19,13 +29,9 @@ def implement_lottery_post_save(
 
     print(f"DELAY DELAY DELAY DELAY DELAY DELAY DELAY {delay}")
 
-    time.sleep(
-        delay
-    )
+    threading.Thread(target=_apply_lottery, args=(delay, )).start()
 
-    print("START LOTTERY")
-
-    implement_lottery()
+    print("START THREAD")
 
 
 post_save.connect(implement_lottery_post_save,
