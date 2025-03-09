06baec5b20ebbca0d2ebd25e7fe408e67eee1f7c
Bogdan Loginov <bogdanloginov31@gmail.com>
2025-03-09 14:59:14 +0300

    Add admin

--- backend/products/products/lottery/admin.py
+++ backend/products/products/lottery/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models.lottery import LotteryEvent, LotteryParticipant
 
-# Register your models here.
+
+@admin.register(LotteryEvent)
+class LotteryEventAdmin(admin.ModelAdmin):
+    list_display = ["is_active", "prize_main", "prize_secondary", "is_dummy"]
+
+
+@admin.register(LotteryParticipant)
+class LotteryParticipantAdmin(admin.ModelAdmin):
+    list_display = ["user_id", "lottery"]
