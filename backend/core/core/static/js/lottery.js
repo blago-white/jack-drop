5eb846d0d3249383228896642f3a1eb44795c16e
Bogdan Loginov <bogdanloginov31@gmail.com>
2025-03-11 19:21:25 +0300

    Add hovering effects

--- backend/core/core/static/js/lottery.js
+++ backend/core/core/static/js/lottery.js
@@ -4,15 +4,15 @@ function renderLotteryInfo(renderMainPrize) {
         <h3 class="lottery-expand-header">РАЗДАЧА<br>СКИНОВ
             <button class="lottery-expand-close-btn" onclick="return reduceLotteryInfo();">X</button>
         </h3>
-        <div class="lottery-prize-info">
+        <div class="lottery-prize-info lotterty-info-row">
             <img src="https://cdn.rust.tm/item/AK+Royale/300.png" class="lottery-prize-img">
             <span class="lottery-prize-name">StatTrak™ MAC-10 | Neon Rider</span>
         </div>
-        <div class="lottery-requirement">
+        <div class="lottery-requirement lotterty-info-row">
             <span>Пополните свой баланс от 30 000₽<br>Ваш баланс: 0₽</span>
             <button class="lottery-requirement-replenish-btn">ПОПОЛНИТЬ</button>
         </div>
-        <div class="lottery-stats">
+        <div class="lottery-stats lotterty-info-row">
             <div class="timer lottery-timer" style="">
             <div class="timer-ring lottery-expand-ring" id="timerRing"></div>
                 <div class="timer-data">
@@ -22,7 +22,7 @@ function renderLotteryInfo(renderMainPrize) {
             </div>
             <span class="lottery-expand-partipicant-count">УЧАСТНИКОВ: 24</span>
         </div>
-        <button class="lottery-make-part-btn">УЧАСТВОВАТЬ</button>
+        <button class="lottery-make-part-btn lotterty-info-row">УЧАСТВОВАТЬ</button>
     </div>
     `;
 
@@ -42,10 +42,9 @@ function reduceLotteryInfo() {
     document.getElementById("prize-wrappper").style.backgroundColor = "rgb(20, 20, 20, .9)";
 
     setTimeout(() => {
-        document.getElementById("prize-wrappper").style = "transition: all .2s ease;";
-
-        document.getElementById("prize-wrappper").style.visibility = "visible";
-        document.getElementById("prize-wrappper").style.display = "flex";
+        document.getElementById("prize-wrappper").innerHTML = "";
+        document.getElementById("prize-wrappper").style.visibility = "hidden";
+        document.getElementById("prize-wrappper").style.display = "none";
     }, 200);
 }
 
