abeecebc4dd7f9adf51fc65f3990173c8610b012
Bogdan Loginov <bogdanloginov31@gmail.com>
2025-03-13 13:05:32 +0300

    Add admin view

--- backend/products/products/games/admin.py
+++ backend/products/products/games/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 
-from .models import GameResult
+from .models.results import GameResult
+from .models.fortune import FortuneWheelBan
 
 
 @admin.register(GameResult)
 class GameResultAdmin(admin.ModelAdmin):
     list_display = ["get_game_display", "is_win", "date"]
+
+
+@admin.register(FortuneWheelBan)
+class FortuneWheelBanService(admin.ModelAdmin):
+    list_display = ["user_id"]
--- backend/products/products/games/models/fortune.py
+++ backend/products/products/games/models/fortune.py
@@ -3,3 +3,6 @@ from django.db import models
 
 class FortuneWheelBan(models.Model):
     user_id = models.IntegerField()
+
+    def __str__(self):
+        return f"Banned: {self.user_id}"
